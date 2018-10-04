14797116147f098508da43df77c2caf4a000c210
Josue Abraham <josuea@noukod.com>
2018-10-04 15:03:57 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,12 +11,12 @@ class Author
   @@all
   end
   def add_post(post)
-    post.author = self
-  end
+  #   post.author = self
+  # end
 
     post = Post.new(title)
     post.author = self
-
+  end
   def posts
     Post.all.select {|post| post.aunthor == self}
   end
